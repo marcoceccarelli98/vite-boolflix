06a47a84e9f78c1003b581fe63cf644a3f7ceb84
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-05 12:44:41 +0200

    [Milestone 0]

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+#TMDB Key : b875ad765049fffffbab8ac9601a041f
--- src/data/store.js
+++ src/data/store.js
@@ -2,5 +2,10 @@ import { reactive } from "vue";
 
 export const store = reactive({
   loading: false,
-  apiUrl: "",
+  apiUrl: "https://developers.themoviedb.org/3",
+  apiKey: "?api_key=b875ad765049fffffbab8ac9601a041f",
+  apiSearch: "/search",
+  apiMovie: "/movie",
+  apiQuery: "&query=",
+  inputSearch: "",
 });
