a94a1b776011851455ffca1d295259db7694ecb9
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-06 11:13:41 +0200

    [UPD] Add comments to store

--- src/data/store.js
+++ src/data/store.js
@@ -1,14 +1,17 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+  // API
   loading: false,
   apiUrl: "https://api.themoviedb.org/3",
   apiKey: "b875ad765049fffffbab8ac9601a041f",
   apiSearchMovie: "/search/movie",
   apiSearchSeries: "/search/tv",
-  apiQuery: "&query=",
+
+  // INPUTS
   inputSearch: "",
 
+  // DATA
   movies: [],
   series: [],
 });
